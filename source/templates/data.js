965d2132e37e5d3849210216608b4b6043f67ae0
VasiliCheb <skimanvasia@gmail.com>
2022-06-27 19:00:45 +0300

    Разметил Хедер и Футер

--- source/templates/data.js
+++ source/templates/data.js
@@ -1,7 +1,8 @@
 export default {
   "pages": [
     { "id": 0, "path": "index.html", "name": "Главная" },
-    { "id": 1, "path": "catalog.html", "name": "Каталог продукции" },
-    { "id": 2, "path": "form.html", "name": "Подбор программы" }
+    { "id": 1, "path": "catalog.html", "name": "Каталог" },
+    { "id": 2, "path": "delivery.html", "name": "Доставка" },
+    { "id": 3, "path": "contacts.html", "name": "О нас" }
   ]
 }
